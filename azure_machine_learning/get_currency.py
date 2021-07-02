2b0ec996a2bd4d11226fe2546bb4375a40168502
KuiMing <benjamin0901@gmail.com>
2021-07-02 16:57:59 +0800

    get recent data from investing.com and register in azureml

--- azure_machine_learning/get_currency.py
+++ azure_machine_learning/get_currency.py
@@ -1,25 +1,82 @@
+import argparse
+from collections import deque
 from datetime import datetime
+from io import StringIO
+import os
 import pickle
+import pandas as pd
 import investpy
 from sklearn.preprocessing import MinMaxScaler
+from azureml.core import Workspace, Run, Dataset
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--history", type=bool, default=False)
+    parser.add_argument(
+        "--target_path",
+        help="file folder in datastore",
+        type=str,
+        default="datasets/currency",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def get_partial_csv(path, rows):
+    with open(path, "r") as f_h:
+        que = deque(f_h, rows)
+    f_h.close()
+    history = pd.read_csv(
+        StringIO("".join(que)),
+        header=None,
+        names=["Date", "Open", "High", "Low", "Close", "Currency"],
+    )
+    return history
 
 
 def main():
-    usd_twd = investpy.get_currency_cross_historical_data(
-        "USD/TWD", from_date="01/01/1900", to_date=datetime.now().strftime("%d/%m/%Y")
-    )
-    usd_twd.reset_index(inplace=True)
-    usd_twd.to_csv("currency/usd_twd.csv", index=False)
-    currency_data = usd_twd.Close.values.reshape(-1, 1)
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    currency_data = scaler.fit_transform(currency_data)
-    with open("currency/usd_twd.pickle", "wb") as f_h:
-        pickle.dump(scaler, f_h)
-    f_h.close()
-    currency_data = usd_twd[
-        (usd_twd.Date >= "2010-01-01") & (usd_twd.Date < "2021-01-01")
-    ]
-    currency_data.to_csv("currency/traing_data.csv")
+    args = parse_args()
+    if args.history:
+        usd_twd = investpy.get_currency_cross_historical_data(
+            "USD/TWD",
+            from_date="01/01/1900",
+            to_date=datetime.now().strftime("%d/%m/%Y"),
+        )
+        usd_twd.reset_index(inplace=True)
+        usd_twd.to_csv("currency/usd_twd.csv", index=False)
+        currency_data = usd_twd.Close.values.reshape(-1, 1)
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        currency_data = scaler.fit_transform(currency_data)
+        with open("currency/usd_twd.pickle", "wb") as f_h:
+            pickle.dump(scaler, f_h)
+        f_h.close()
+        currency_data = usd_twd[
+            (usd_twd.Date >= "2010-01-01") & (usd_twd.Date < "2021-01-01")
+        ]
+        currency_data.to_csv("currency/traing_data.csv")
+    else:
+        path = os.path.join(args.target_folder, "usd_twd.csv")
+        history = get_partial_csv(path, 2)
+
+        recent = investpy.get_currency_cross_recent_data("USD/TWD")
+        recent.reset_index(inplace=True)
+        recent = recent[
+            ~recent.Date.isin(
+                list(history.Date.values) + [datetime.now().strftime("%Y-%m-%d")]
+            )
+        ]
+        recent.to_csv(path, header=False, index=False, mode="a")
+        history = get_partial_csv(path, 2400)
+        history.to_csv(os.path.join(args.target_folder, "traing_data.csv"), index=False)
+        run = Run.get_context()
+        try:
+            work_space = run.experiment.workspace
+        except AttributeError:
+            work_space = Workspace.from_config()
+        datastore = work_space.get_default_datastore()
+        dataset = Dataset.File.from_files(path=(datastore, args.target_path))
+        dataset.register(work_space, name=args.target_path.split("/")[1])
 
 
 if __name__ == "__main__":
