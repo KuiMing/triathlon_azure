2a1a273af55b4b9e949fdd25804117e4adf39635
KuiMing <benjamin0901@gmail.com>
2021-07-04 14:46:43 +0800

    correct modle file name

--- azure_machine_learning/predict_currency.py
+++ azure_machine_learning/predict_currency.py
@@ -17,7 +17,7 @@ def init():
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
     # For multiple models, it points to the folder containing all deployed models (./azureml-models)
-    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "keras_lenet.h5")
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "keras_lstm.h5")
     model = load_model(model_path)
     scaler_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "scaler.pickle")
     with open(scaler_path, "rb") as f_h:
